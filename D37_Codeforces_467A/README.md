f681fa2601db87cf37eabb409b936a11235189ac
ZetsuX <kevnhalim@gmail.com>
2023-05-20 13:58:36 +0700

    feat: add 39th problem

--- D37_Codeforces_467A/README.md
+++ D37_Codeforces_467A/README.md
@@ -1,5 +1,7 @@
 # A. George and Accommodation
 
+Source : [Problem](https://codeforces.com/problemset/problem/467/A)
+
 -   time limit per test 1 second
 -   memory limit per test 256 megabytes
 -   input standard input
---
+++ D39_Codeforces_136A/Codeforces_136A.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <math.h>
+#define gc getchar
+#define pc putchar
+
+inline void numput (int n)
+{
+  int N = n, rev, count = 0;
+  rev = N;
+  if (N == 0) {
+    pc('0'); 
+    pc('\n'); 
+    return;
+  }
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc(' ');
+}
+
+int numget()
+{
+  int value = 0; bool ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=1;
+  return value;
+}
+ 
+void solve()
+{
+  int n = numget();
+  int arr[n+1];
+
+  for (int i = 1 ; i <= n ; i++) {
+    int p = numget();
+    arr[p] = i;
+  }
+
+  for (int i = 1 ; i <= n ; i++) {
+    numput(arr[i]);
+  }
+  puts("");
+}
+ 
+int main()
+{
+  solve();
+  return 0;
+}
