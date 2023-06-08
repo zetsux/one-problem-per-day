be6fb165dc63a701a0ffdf98ef7e814b86c99d0d
ZetsuX <kevnhalim@gmail.com>
2023-06-08 11:31:05 +0700

    feat: add 58th problem

---
+++ D58_Codeforces_581A/Codeforces_581A.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#define gc getchar
+#define pc putchar
+
+inline void numput (int n)
+{
+  int N = n, rev, count = 0;
+  rev = N;
+  if (N == 0) {
+    pc('0'); 
+    pc(' '); 
+    return;
+  }
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc(' ');
+}
+
+int numget()
+{
+  int value = 0; bool ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=1;
+  return value;
+}
+
+void outputRes(int mn, int mx) {
+    numput(mn), numput((mx - mn) >> 1);
+}
+ 
+void solve()
+{
+  int l = numget(), r = numget();
+  (l > r ? outputRes(r, l) : outputRes(l, r));
+}
+ 
+int main()
+{
+  solve();
+  return 0;
+}
