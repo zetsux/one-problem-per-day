8adb119a17e2ca63ea0cdd2c8f9d3a9681fa8010
Fehlling Halim <kevnhalim@gmail.com>
2023-07-02 02:03:59 +0700

    feat: add 82nd problem

---
+++ D82_Codeforces_233A/Codeforces_233A.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#define gc getchar
+#define pc putchar
+#define ull unsigned long long
+
+inline void numput (int n)
+{
+  int N = n, rev, count = 0;
+  rev = N;
+  if (N == 0) {
+    pc('0'); 
+    pc(' '); 
+    return;
+  }
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc(' ');
+}
+
+int numget()
+{
+  int value = 0; bool ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=1;
+  return value;
+}
+
+void solve()
+{
+  int x = numget();
+  if (x & 1) {
+    printf("-1");
+    return;
+  }
+
+  int arr[x], h = x/2;
+  for (int i = 0 ; i < x ; i++) arr[i] = i + 1;
+  for (int i = 0 ; i < h ; i++) {
+    int tmp = arr[i];
+    arr[i] = arr[i + h];
+    arr[i + h] = tmp;
+  }
+  for (int i = 0 ; i < x ; i++) numput(arr[i]);
+}
+ 
+int main()
+{
+  solve();
+  return 0;
+}
