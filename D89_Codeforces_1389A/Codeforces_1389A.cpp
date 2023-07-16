55d6f274955ef6625bbb9725ffd168c99170b5ab
Fehlling Halim <kevnhalim@gmail.com>
2023-07-17 00:18:07 +0700

    feat: add 89th problem

---
+++ D89_Codeforces_1389A/Codeforces_1389A.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#define gc getchar
+#define pc putchar
+#define ull unsigned long long
+
+inline void numput (int n)
+{
+  int N = n, rev, count = 0;
+  rev = N;
+  if (N == 0) {
+    pc('0'); 
+    pc(' '); 
+    return;
+  }
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc(' ');
+}
+
+int numget()
+{
+  int value = 0; bool ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=1;
+  return value;
+}
+
+void solve()
+{
+  int n = numget();
+  while (n--) {
+    int l = numget(), r = numget();
+    int l2 = l*2;
+
+    if (r >= l2) {
+      numput(l);
+      numput(l2);
+      pc('\n');
+    } else puts("-1 -1");
+  }
+}
+ 
+int main()
+{
+  solve();
+  return 0;
+}
