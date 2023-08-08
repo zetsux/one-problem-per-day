e428521d493f459c8a323241ae88314339e9f756
Fehlling Halim <kevnhalim@gmail.com>
2023-08-08 14:55:32 +0700

    feat: add 111th problem

---
+++ D111_Codeforces_1702A/Codeforces_1702A.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#define gc getchar
+#define pc putchar
+#define ull unsigned long long
+
+inline void numput (int n)
+{
+  int N = n, rev, count = 0;
+  rev = N;
+  if (N == 0) {
+    pc('0'); 
+    pc('\n'); 
+    return;
+  }
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc('\n');
+}
+
+int numget()
+{
+  int value = 0; bool ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=1;
+  return value;
+}
+
+void solve()
+{
+  int n = numget();
+  while (n--) {
+    int x = numget();
+    double d = 0.1;
+    int tmp = x;
+    while (tmp > 0) {
+      d *= 10;
+      tmp /= 10;
+    }
+    numput(x - d);
+  }
+}
+ 
+int main()
+{
+  solve();
+  return 0;
+}
