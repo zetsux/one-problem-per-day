a92c8d8fa734e0fd0cc2c13a5c73aba436cda9ad
Fehlling Halim <kevnhalim@gmail.com>
2023-08-22 16:00:28 +0700

    feat: add 125th problem

---
+++ D125_Codeforces_1490A/Codeforces_1490A.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#define gc getchar
+#define pc putchar
+#define ull unsigned long long
+
+inline void numput (int n)
+{
+  int N = n, rev, count = 0;
+  rev = N;
+  if (N == 0) {
+    pc('0'); 
+    pc('\n'); 
+    return;
+  }
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc('\n');
+}
+
+int numget()
+{
+  int value = 0; bool ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=1;
+  return value;
+}
+
+void solve()
+{
+  int n = numget();
+  while (n--) {
+    int x = numget(), cnt = 0;
+    int a = numget();
+    for (int i = 1; i < x ; i++) {
+      int b = numget();
+      double mx, mn;
+      if (a > b) {
+        mx = a;
+        mn = b;
+      } else {
+        mx = b;
+        mn = a;
+      }
+
+      while (mx / mn > 2) {
+        mx = ceil(mx / 2);
+        cnt++;
+      }
+      a = b;
+    }
+    numput(cnt);
+  }
+}
+ 
+int main()
+{
+  solve();
+  return 0;
+}
