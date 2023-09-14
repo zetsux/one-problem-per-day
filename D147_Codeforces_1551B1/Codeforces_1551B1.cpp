5ce69d4dd8fae8ab681214e27692ffc2f4f8afc5
Fehlling Halim <kevnhalim@gmail.com>
2023-09-14 08:51:54 +0700

    feat: add 147th problem

---
+++ D147_Codeforces_1551B1/Codeforces_1551B1.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#define gc getchar
+#define pc putchar
+#define ull unsigned long long
+
+inline void numput (ull n)
+{
+  ull N = n, rev, count = 0;
+  rev = N;
+  if (N == 0) {
+    pc('0'); 
+    pc('\n'); 
+    return;
+  }
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc('\n');
+}
+
+ull numget()
+{
+  ull value = 0; bool ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=1;
+  return value;
+}
+
+void solve()
+{
+  int t = numget();
+  while (t--) {
+    char str[51];
+    gets(str);
+
+    int arr[26] = {0}, res = 0, cnt = 0;
+    for (int i = 0 ; i < strlen(str) ; i++) {
+      int nm = str[i] - 'a';
+      arr[nm]++;
+    }
+
+    for (int i = 0 ; i < 26 ; i++) {
+      if (arr[i] >= 2) res++;
+      else cnt += arr[i];
+    }
+    numput(res + (cnt/2));
+  }
+}
+ 
+int main()
+{
+  solve();
+  return 0;
+}
