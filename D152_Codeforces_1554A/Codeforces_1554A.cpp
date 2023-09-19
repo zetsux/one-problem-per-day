f6b6a0b667b906689443ae4d9efdf35b78f51b2e
Fehlling Halim <kevnhalim@gmail.com>
2023-09-19 16:31:52 +0700

    feat: add 152nd problem

---
+++ D152_Codeforces_1554A/Codeforces_1554A.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#define gc getchar
+#define pc putchar
+#define ull unsigned long long
+
+inline void numput (ull n)
+{
+  ull N = n, rev, count = 0;
+  rev = N;
+  if (N == 0) {
+    pc('0'); 
+    pc('\n'); 
+    return;
+  }
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc('\n');
+}
+
+ull numget()
+{
+  ull value = 0; bool ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=1;
+  return value;
+}
+
+void solve()
+{
+  int t = numget();
+  while (t--) {
+    int x = numget();
+    ull n1 = numget(), n2, mx = 0;
+    for (int i = 1 ; i < x ; i++) {
+      n2 = numget();
+      ull tmp = n1 * n2;
+      mx = (tmp > mx ? tmp : mx);
+      n1 = n2;
+    }
+    numput(mx);
+  }
+}
+ 
+int main()
+{
+  solve();
+  return 0;
+}
