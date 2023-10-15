42958d489f47f7c0afa13138cf3cf752894fbaa1
Fehlling Halim <kevnhalim@gmail.com>
2023-10-16 00:09:01 +0700

    feat: add 178th problem

---
+++ D178_Codeforces_1196A/Codeforces_1196A.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#define gc getchar
+#define pc putchar
+#define ll long long
+#define ull unsigned long long
+
+inline void numput (ll n)
+{
+  ll N = n, rev, count = 0;
+  if (N == 0) {
+    pc('0'); 
+    pc('\n'); 
+    return;
+  }
+
+  if (N < 0) {
+    N = -N;
+    pc('-');
+  }
+  rev = N;
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc('\n');
+}
+
+ll numget()
+{
+  ll value = 0; bool ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=-1;
+  return value;
+}
+
+int countDiff(const char* str1, const char* str2, int n) {
+  int cnt = 0;
+  for (int i = 0; i < n; i++) cnt += (abs(str1[i] - str2[i]));
+  return cnt;
+}
+
+void solve()
+{
+  int t = numget();
+  while (t--) {
+    ull sum = 0;
+    for (ull i = 0; i < 3; i++) sum += numget();
+    numput(sum/2);
+  }
+}
+ 
+int main()
+{
+  solve();
+  return 0;
+}
