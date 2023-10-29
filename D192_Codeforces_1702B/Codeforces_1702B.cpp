4c617f92bde4926dbd70510422f59f1927fd7516
Fehlling Halim <kevnhalim@gmail.com>
2023-10-30 00:36:24 +0700

    feat: add 192nd problem

---
+++ D192_Codeforces_1702B/Codeforces_1702B.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#define gc getchar
+#define pc putchar
+#define ll long long
+#define ull unsigned long long
+
+inline void numput (ll n)
+{
+  ll N = n, rev, count = 0;
+  if (N == 0) {
+    pc('0'); 
+    pc('\n'); 
+    return;
+  }
+
+  if (N < 0) {
+    N = -N;
+    pc('-');
+  }
+  rev = N;
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc('\n');
+}
+
+ll numget()
+{
+  ll value = 0; bool ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=-1;
+  return value;
+}
+
+void solve()
+{
+  int t = numget();
+  while (t--) {
+    int cur[3] = {0}, cnt = 1, idx = 0;
+
+    char c = gc();
+    while (c != '\n' && c != EOF) {
+      int flag = 1;
+      for (int j = 0; j < 3; j++) {
+        if (cur[j] == (c - 96)) {
+          flag = 0;
+          break;
+        }
+      }
+
+      if (flag) {
+        if (idx >= 3) {
+          cnt++;
+          idx = cur[1] = cur[2] = 0;
+        } cur[idx++] = c - 96;
+      } c = gc();
+    } numput(cnt);
+  }
+}
+ 
+int main()
+{
+  solve();
+  return 0;
+}
