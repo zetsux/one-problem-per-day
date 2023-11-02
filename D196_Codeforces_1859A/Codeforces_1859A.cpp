558745b752fe8bce7bd691140aa213439abdf336
Fehlling Halim <kevnhalim@gmail.com>
2023-11-03 01:10:20 +0700

    feat: add 196th problem

---
+++ D196_Codeforces_1859A/Codeforces_1859A.cpp
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#define gc getchar
+#define pc putchar
+#define ll long long
+#define ull unsigned long long
+
+inline void numput (ll n)
+{
+  ll N = n, rev, count = 0;
+  if (N == 0) {
+    pc('0'); 
+    pc(' '); 
+    return;
+  }
+
+  if (N < 0) {
+    N = -N;
+    pc('-');
+  }
+  rev = N;
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc(' ');
+}
+
+ll numget()
+{
+  ll value = 0; bool ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=-1;
+  return value;
+}
+
+void swap(int* a, int* b)
+{
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+int partition(int arr[], int low, int high)
+{
+  int pivot = arr[high];
+  int i = (low - 1);
+
+  for (int j = low; j <= high - 1; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      swap(&arr[i], &arr[j]);
+    }
+  }
+  swap(&arr[i + 1], &arr[high]);
+  return (i + 1);
+}
+ 
+void quickSort(int arr[], int low, int high)
+{
+  if (low < high) {
+    int pi = partition(arr, low, high);
+    quickSort(arr, low, pi - 1);
+    quickSort(arr, pi + 1, high);
+  }
+}
+
+void solve()
+{
+  int t = numget();
+  while (t--) {
+    int n = numget();
+    int arr[n];
+    for (int i = 0 ; i < n ; i++) arr[i] = numget();
+    quickSort(arr, 0, n - 1);
+    if (arr[0] == arr[n - 1]) {
+      puts("-1");
+      continue;
+    }
+
+    int idx = 0;
+    while (arr[idx] == arr[0]) idx++;
+
+    numput(idx);
+    numput(n - idx);
+    pc('\n');
+
+    for (int i = 0 ; i < idx ; i++) numput(arr[i]);
+    pc('\n');
+    for (;idx < n ; idx++) numput(arr[idx]);
+    pc('\n');
+  }
+}
+ 
+int main()
+{
+  solve();
+  return 0;
+}
