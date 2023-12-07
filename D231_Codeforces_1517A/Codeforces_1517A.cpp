14649f329ae664545881b8d410608e55aff26f98
Fehlling Halim <kevnhalim@gmail.com>
2023-12-08 01:23:16 +0700

    feat: add 231st problem

---
+++ D231_Codeforces_1517A/Codeforces_1517A.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <algorithm>
+#define gc getchar
+#define pc putchar
+#define ll long long
+#define ull unsigned long long
+
+inline void numput (ll n)
+{
+  ll N = n, rev, count = 0;
+  if (N == 0) {
+    pc('0'); 
+    pc('\n'); 
+    return;
+  }
+
+  if (N < 0) {
+    N = -N;
+    pc('-');
+  }
+  rev = N;
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc('\n');
+}
+
+ll numget()
+{
+  ll value = 0; int ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=-1;
+  return value;
+}
+
+void solve()
+{
+  ll x = numget();
+  if (x % 2050 == 0) {
+    ll d = x / 2050, sm = 0;
+    while (d > 0) {
+      ll tmp = d % 10;
+      sm += tmp; d /= 10;
+    } numput(sm);
+  } else puts("-1");
+}
+ 
+int main()
+{
+  int t = numget();
+  while (t--) solve();
+  return 0;
+}
---
+++ D231_Codeforces_1517A/README.md
@@ -0,0 +1,51 @@
+# A. Sum of 2050
+
+Source : [Problem](https://codeforces.com/problemset/problem/1517/A)
+
+- time limit per test 1 second
+- memory limit per test 256 megabytes
+- input standard input
+- output standard output
+
+A number is called 2050-number if it is 2050, 20500, ..., (2050 ⋅ 10k for integer k ≥ 0).
+
+Given a number n, you are asked to represent n as the sum of some (not necessarily distinct) 2050-numbers. Compute the minimum number of 2050-numbers required for that.
+
+## Input
+
+The first line contains a single integer T (1 ≤ T ≤ 1000) denoting the number of test cases.
+
+The only line of each test case contains a single integer n (1 ≤ n ≤ 10^18) denoting the number to be represented.
+
+## Output
+
+For each test case, output the minimum number of 2050-numbers in one line.
+
+If n cannot be represented as the sum of 2050-numbers, output −1 instead.
+
+## Example
+
+### Sample Input 1
+
+    6
+    205
+    2050
+    4100
+    20500
+    22550
+    25308639900
+
+### Sample Output 1
+
+    -1
+    1
+    2
+    1
+    2
+    36
+
+## Note
+
+In the third case, 4100 = 2050 + 2050.
+
+In the fifth case, 22550 = 20500 + 2050.
