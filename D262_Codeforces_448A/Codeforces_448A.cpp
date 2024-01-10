cdce4fc2b2d976830a94ec79dc8cac10877c11c3
Fehlling Halim <kevnhalim@gmail.com>
2024-01-10 10:44:16 +0700

    feat: add 262nd problem

---
+++ D262_Codeforces_448A/Codeforces_448A.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <algorithm>
+#define gc getchar
+#define pc putchar
+#define ll long long
+#define ull unsigned long long
+
+inline void numput (ll n)
+{
+  ll N = n, rev, count = 0;
+  if (N == 0) {
+    pc('0'); 
+    pc('\n'); 
+    return;
+  }
+
+  if (N < 0) {
+    N = -N;
+    pc('-');
+  }
+  rev = N;
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc('\n');
+}
+
+ll numget()
+{
+  ll value = 0; int ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=-1;
+  return value;
+}
+
+void solve()
+{
+  int a = 0, b = 0, n;
+  for (int i = 0; i < 3 ; i++) a += numget();
+  for (int i = 0; i < 3 ; i++) b += numget();
+  n = numget();
+
+  a = ceil((double) a/5);
+  b = ceil((double) b/10);
+  puts(n < (a+b) ? "NO" : "YES");
+}
+
+int main()
+{
+  solve();
+  return 0;
+}
