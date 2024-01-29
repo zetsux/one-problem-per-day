c7c901c9ec3e838db518b97c33b058d799eb4f40
Fehlling Halim <kevnhalim@gmail.com>
2024-01-30 02:13:59 +0700

    feat: add 281st problem

---
+++ D281_Codeforces_1809A/Codeforces_1809A.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <algorithm>
+#define gc getchar
+#define pc putchar
+#define ll long long
+#define ull unsigned long long
+
+inline void numput (ll n)
+{
+  ll N = n, rev, count = 0;
+  if (N == 0) {
+    pc('0'); 
+    pc('\n'); 
+    return;
+  }
+
+  if (N < 0) {
+    N = -N;
+    pc('-');
+  }
+  rev = N;
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc('\n');
+}
+
+ll numget()
+{
+  ll value = 0; int ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=-1;
+  return value;
+}
+
+void solve()
+{
+  int arr[10] = {0}, mx = 0;
+  for (int i = 0 ; i < 4 ; i++) {
+    int n = gc() - '0';
+    if (++arr[n] > mx) mx = arr[n];
+  }
+
+  switch (mx) {
+    case 3:
+      puts("6"); break;
+    case 4:
+      puts("-1"); break;
+    default:
+      puts("4");
+  } gc();
+}
+
+int main()
+{
+  int t = numget();
+  while (t--) solve();
+  return 0;
+}
