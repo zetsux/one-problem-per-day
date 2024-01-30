b3977decbb199e88babf10211347765728d974c6
Fehlling Halim <kevnhalim@gmail.com>
2024-01-31 01:05:36 +0700

    feat: add 282nd problem

---
+++ D282_Codeforces_1206A/Codeforces_1206A.cpp
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <algorithm>
+#define gc getchar
+#define pc putchar
+#define ll long long
+#define ull unsigned long long
+
+inline void numput (ll n)
+{
+  ll N = n, rev, count = 0;
+  if (N == 0) {
+    pc('0'); 
+    pc(' '); 
+    return;
+  }
+
+  if (N < 0) {
+    N = -N;
+    pc('-');
+  }
+  rev = N;
+  while ((rev % 10) == 0) {
+    count++;
+    rev /= 10;
+  }
+  rev = 0;
+  while (N != 0) { 
+    rev = (rev<<3) + (rev<<1) + N % 10;
+    N /= 10;
+  }  
+  while (rev != 0) {
+    pc(rev % 10 + '0'); 
+    rev /= 10;
+  }
+  while (count--) pc('0');
+  pc(' ');
+}
+
+ll numget()
+{
+  ll value = 0; int ne = 0;
+  char c = gc();
+  while(c==' ' || c == '\n') c = gc();
+  if(c == '-'){ne = 1; c = gc();}
+  while(c>='0' && c<='9'){
+      value = (value<<3) + (value<<1) + c - '0'; c = gc();}
+  if(ne) value*=-1;
+  return value;
+}
+
+void solve()
+{
+  int n = numget(), mx1 = 0, mx2 = 0;
+  for (int i = 0; i < n; i++) {
+    int nm = numget();
+    if (nm > mx1) mx1 = nm;
+  }
+
+  int m = numget();
+  for (int i = 0; i < m; i++) {
+    int nm = numget();
+    if (nm > mx2) mx2 = nm;
+  } numput(mx1); numput(mx2);
+}
+
+int main()
+{
+  solve();
+  return 0;
+}
