6a417a659afdd04309e87ca4ff4b90cab9476831
zetsux <kevnhalim@gmail.com>
2024-08-30 23:01:51 +0700

    feat: add 408th problem

---
+++ D408_LeetCode_9/LeetCode_9.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x == 0) return true;
+        else if (x < 0 || x % 10 == 0) return false;
+
+        int rev = 0;
+        while (x > rev) {
+            rev = (rev * 10) + (x % 10);
+            x /= 10;
+        } return (x == rev || x == (rev / 10));
+    }
+};
