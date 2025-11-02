e91dcb62ef9413733599658c31127c186162a5f2
zetsux <kevnhalim@gmail.com>
2025-11-02 12:50:40 +0700

    feat: add 455th problem

---
+++ D455_LeetCode_206/LeetCode_206.go
@@ -0,0 +1,21 @@
+package d455leetcode206
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+
+	for cur != nil {
+		next := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = next
+	}
+
+	return prev
+}
