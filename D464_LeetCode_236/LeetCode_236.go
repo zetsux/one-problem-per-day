3b78b8cbcfd64d9ebc49f1fea37a727ccaca11ce
zetsux <kevnhalim@gmail.com>
2025-11-11 12:00:01 +0700

    feat: add 464th problem

---
+++ D464_LeetCode_236/LeetCode_236.go
@@ -0,0 +1,25 @@
+package d464leetcode236
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root == p || root == q {
+		return root
+	}
+
+	l := lowestCommonAncestor(root.Left, p, q)
+	r := lowestCommonAncestor(root.Right, p, q)
+
+	if l != nil {
+		if r != nil {
+			return root
+		}
+		return l
+	}
+	return r
+}
