d0008129be81b739353c236a27d766d422a73a47
zetsux <kevnhalim@gmail.com>
2025-11-12 12:10:12 +0700

    feat: add 465th problem

---
+++ D465_LeetCode_199/LeetCode_199.go
@@ -0,0 +1,39 @@
+package d465leetcode199
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	result := []int{}
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			// if last node at this level, record it
+			if i == size-1 {
+				result = append(result, node.Val)
+			}
+
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+
+	return result
+}
