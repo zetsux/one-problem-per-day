0cc729f326f8765d13465fe748d8be866f26947c
zetsux <kevnhalim@gmail.com>
2025-12-16 15:57:21 +0700

    feat: add 483rd problem

---
+++ D483_LeetCode_2/LeetCode_2.go
@@ -0,0 +1,32 @@
+package d483leetcode2
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{}
+	curr := head
+	carry := 0
+
+	for l1 != nil || l2 != nil || carry != 0 {
+		sum := carry
+
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+
+		carry = sum / 10
+		curr.Next = &ListNode{Val: sum % 10}
+		curr = curr.Next
+	}
+
+	return head.Next
+}
