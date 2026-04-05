d693d4868c96c9cd8c50770e1bc9f11969bf46d6
Kevin Nathanael Halim <108170234+zetsux@users.noreply.github.com>
2026-04-05 08:08:21 +0700

    feat: add 515th problem (#939)

---
+++ D515_LeetCode_278/LeetCode_278.go
@@ -0,0 +1,22 @@
+package d515leetcode278
+
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+	l, r := 1, n
+	for l < r {
+		m := (l + r) / 2
+		if isBadVersion(m) {
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+	return l
+}
